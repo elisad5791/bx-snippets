f8a7bb7f42cab4b51252b7c18cd5375ac4503e3d
elisad5791 <elisad5791@mail.ru>
2024-06-23 00:43:06 +0500

    commit

---
+++ local/php_interface/migrations/doctors_elements20240622223722.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Sprint\Migration;
+
+class doctors_elements20240622223722 extends Version
+{
+    protected $author = "admin";
+
+    protected $description   = "";
+
+    protected $moduleVersion = "4.9.8";
+
+    /**
+     * @throws Exceptions\MigrationException
+     * @throws Exceptions\RestartException
+     * @return bool|void
+     */
+    public function up()
+    {
+        $this->getExchangeManager()
+             ->IblockElementsImport()
+             ->setExchangeResource('iblock_elements.xml')
+             ->setLimit(20)
+             ->execute(function ($item) {
+                 $this->getHelperManager()
+                      ->Iblock()
+                      ->addElement(
+                          $item['iblock_id'],
+                          $item['fields'],
+                          $item['properties']
+                      );
+             });
+    }
+
+    /**
+     * @throws Exceptions\MigrationException
+     * @throws Exceptions\RestartException
+     * @return bool|void
+     */
+    public function down()
+    {
+    }
+}
