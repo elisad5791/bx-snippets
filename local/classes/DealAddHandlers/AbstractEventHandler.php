7a5c2ed5cd9743c08587307a8b94056d87e05385
elisad5791 <elisad5791@mail.ru>
2024-07-03 00:23:41 +0500

    commit

--- local/classes/DealAddHandlers/AbstractEventHandler.php
+++ local/classes/DealAddHandlers/AbstractEventHandler.php
@@ -7,6 +7,9 @@ abstract class AbstractEventHandler
     public static function onDealAddDispatcher($fields)
     {
         $handler = EventHandlerFactory::create($fields['TYPE_ID']);
+        if (!$handler) {
+            return;
+        }
         $handler->onAfterAdd($fields);
     }
 }
